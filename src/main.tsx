282c2fce61d48404ee1152970db1e031e2eed533
Risekou1 <temka4705@gmail.com>
2026-09-25 21:10:03 +0900

    tmk

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "../app/page";
+import FingerCountPage from "../app/fingers/page";
 import "../app/globals.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("AirDraw root element is missing.");
 createRoot(root).render(
   <React.StrictMode>
-    <Home />
+    {window.location.pathname.replace(/\/$/, "") === "/fingers" ? (
+      <FingerCountPage />
+    ) : (
+      <Home />
+    )}
   </React.StrictMode>,
 );
